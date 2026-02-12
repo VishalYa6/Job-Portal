3dbc622b8a437e7ea86b11f8ce2ab8b0e87c9ef3
VishalYa6 <vy372984@gmail.com>
2026-02-12 22:07:45 +0530

    cors and env

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173", // Adjust this to your frontend URL
+  origin: "https://job-portal-five-mocha.vercel.app/", // Adjust this to your frontend URL
   credentials: true
 }));
 

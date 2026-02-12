c71e5cd81753ece7ca5370aca3dfbf9f28bffb87
VishalYa6 <vy372984@gmail.com>
2026-02-12 23:35:42 +0530

    application-routes

--- backend/routes/application.routes.js
+++ backend/routes/application.routes.js
@@ -5,17 +5,15 @@ import { getUserDashboard } from "../controllers/user.dashboard.controller.js";
 import { getRecruiterDashboard } from "../controllers/application.recruiter.controller.js";
 
 const router = express.Router();
-// Applicant routes
-router.post("/:jobId", authenticate, authorizeRoles("user"), applyForJob);
-router.get("/:jobId", authenticate, authorizeRoles("recruiter"), getApplicationsForJob);
-router.put("/:jobId", authenticate, authorizeRoles("recruiter"), updateApplicationStatus);
-router.delete("/:applicationId", authenticate, authorizeRoles("user"), deleteApplication);
-
-
-
 //user dashboard route
 router.get("/dashboard/user", authenticate, authorizeRoles("user"), getUserDashboard);
 //recruiter dashboard route
 router.get("/dashboard/recruiter", authenticate, authorizeRoles("recruiter"), getRecruiterDashboard);
+
+// Applicant routes
+router.post("/:jobId", authenticate, authorizeRoles("user"), applyForJob);
+router.get("/:jobId", authenticate, authorizeRoles("recruiter"), getApplicationsForJob);
+router.put("/:applicationId", authenticate, authorizeRoles("recruiter"), updateApplicationStatus);
+router.delete("/:applicationId", authenticate, authorizeRoles("user"), deleteApplication);
 
 export default router;
